dbff953e586c5f7c8957ceefc46d5b319bf8b925
Khalid Kafmaghni <kkafmagh@paul-f4Ar8s4.clusters.42paris.fr>
2024-11-15 16:15:06 +0100

    bonus

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkafmagh <kkafmagh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 14:45:01 by kkafmagh          #+#    #+#             */
+/*   Updated: 2024/11/15 15:28:38 by kkafmagh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: kkafmagh <kkafmagh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 10:32:40 by kkafmagh          #+#    #+#             */
-/*   Updated: 2024/11/15 11:56:47 by kkafmagh         ###   ########.fr       */
+/*   Updated: 2024/11/15 14:31:52 by kkafmagh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	countwords(char *s, char c)
+int	countwords(char const *s, char c)
 {
 	int	i;
 	int	j;
@@ -29,215 +29,78 @@ int	countwords(char *s, char c)
 	}
 	if (s[i] == 0 && s[i - 1] != c)
 		j++;
-	// printf("countwords  : %i\n", j);
 	return (j);
 }
 
-
-
-
-int	nextword(char *str, char c)
-{
-	int	i;
-	int	j;
-
-	j = 0;
-	i = 0;
-	while (str[i] == c)
-	{
-		i++;
-	}
-	while (str[i] != c)
-	{
-		i++;
-		j++;
-	}
-	printf("nextword  : %i\n", j);
-	return (j);
-}
-int	len(char *str)
+int	len(char const *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 		i++;
-	// printf("sortie de len : %i\n", i);
 	return (i);
 }
-// char	*mallocaword(char *str)
-// {
-// 	int		i;
-// 	char	*s;
+char	*mallocaword(char const *str, int start, int end)
+{
+	int		i;
+	char	*s;
 
-// 	i = 0;
-// 	s = malloc(sizeof(char) * (len(str) + 1));
-// 	if (s == NULL || !str[0])
-// 		return (NULL);
-// 	while (str[i])
-// 	{
-// 		s[i] = str[i];
-// 		i++;
-// 	}
-// 	s[i] = '\0';
-// 	return (s);
-// }
+	i = 0;
+	s = malloc(sizeof(char) * (end - start + 1));
+	if (s == NULL || !str[0])
+		return (NULL);
+	while (start < end)
+	{
+		s[i] = str[start];
+		start++;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
 
 char	**ft_split(char const *s, char c)
 {
 	int		i;
 	int		j;
 	int		k;
+	int		index;
 	char	**tab;
-	char	*str2;
 
+	index = -1;
 	i = 0;
-	if (!s)
-		return (NULL);
-	tab = malloc(countwords((char *)s, c) + 1 * sizeof(char *));
-	str2 = malloc(sizeof(char *) * nextword((char *)s, c) + 1);
-	// str2 = malloc(sizeof(char *)*6 + 1);
-	if (!tab)
-		return (NULL);
-	k = 0;
 	j = 0;
-	if (!str2)
-	{
+	k = 0;
+	tab = malloc(sizeof(char *) * (countwords(s, c) + 1));
+	if (!s || !tab)
 		return (NULL);
-	}
-	while (i < len((char *)s))
+	while (i <= len(s))
 	{
-		while (s[i] == c)
-			i++;
-		if (s[i] != '\0')
+		if (s[i] != c && index < 0)
+			index = i;
+		else if ((s[i] == c || i == len(s)) && index >= 0)
 		{
-			str2 = malloc(sizeof(char *) * nextword((char *)s, c) + 1);
-			if (!str2)
-			{
-				// Free memory in case of allocation failure
-				while (k > 0)
-				{
-					free(tab[--k]);
-				}
-				free(tab);
-				return (NULL);
-			}
-			j = 0;
-			while (s[i] != c && s[i] != '\0')
-			{
-				str2[j++] = s[i++];
-			}
-			str2[j] = '\0';
-			// Add str2 to the result array
-			tab[k++] = str2;
+			tab[k] = mallocaword(s, index, i);
+			k++;
+			index = -1;
 		}
-		// if (k < countwords((char *)s, c))
-		// {
-		// 	// printf("combien de mots\n");
-		// 	str2 = malloc(sizeof(char *) * nextword((char *)s, c) + 1);
-		// 	if (!str2)
-		// 	{
-		// 		return (NULL);
-		// 	}
-		// }
-		// while (s[i] != c && s[i] != 0)
-		// {
-		// 	str2[j] = s[i];
-		// 	i++;
-		// 	j++;
-		// }
-		// if (j >= len(str2))
-		// {
-		// 	str2[j] = '\0';
-		// 	tab[k] = str2;
-		// 	str2 = NULL;
-		// 	printf("tab[k] : %s\n", tab[k]);
-		// 	j = 0;
-		// 	k++;
-		// }
-		// i++;
-		// printf("i : %i\n", i);
+		i++;
 	}
-	// free(str2);
 	tab[k] = NULL;
-	printf("tab[0] : %s\n", tab[0]);
-	printf("tab[1] : %s\n", tab[1]);
-	printf("tab[2] : %s\n", tab[2]);
-	printf("tab[3] : %s\n", tab[3]);
+	// printf("tab[0] : %s\n", tab[0]);
+	// printf("tab[1] : %s\n", tab[1]);
+	// printf("tab[2] : %s\n", tab[2]);
+	// printf("tab[3] : %s\n", tab[3]);
 	return (tab);
 }
 
 int	main(void)
 {
-	const char	*str = "   khalid kva   kfnwek   dfdvf ";
+	const char	*str = "khalid ";
 	const char	c = ' ';
 
 	ft_split(str, c);
 	// mallocaword(str);
 	// ft_split(str, ' ');
 }
-
-/*
-1 - parcourir une chaine de caracteres
-2 - calculer le nombre de caracteres
-3 - calculer le nombre de mots
-4 - malloc un tableau pour tout y stocker
-5 - malloc mon mot
-
-*/
-
-// char	**ft_split(char const *s, char c)
-// {
-// 	int		i;
-// 	int		j;
-// 	int		k;
-// 	char	**tab;
-// 	char	*str2;
-
-// 	if (!s[0])
-// 		return (NULL);
-// 	tab = malloc(countwords((char *)s, c) + 1);
-// 	str2 = malloc(nextword((char *)s, c) + 1);
-// 	// free(str2);
-// 	// char *str = (char *)s; // copie de la string d'origine
-// 	k = 0;
-// 	i = 0;
-// 	j = 0;
-// 	// tab = "";
-// 	while (i < len((char *)s))
-// 	{
-// 		// if (s[i] != c && (s[i - 1] == c || i == 0))
-// 		// {
-// 		// 	str2 = malloc(nextword((char *)s, c) + 1);
-// 		// }
-// 		// if (s[i] == c)
-// 		// {
-// 		// 	// str++;
-// 		// }
-// 		if (s[i] != c)
-// 		{
-// 			str2[j] = s[i];
-// 			// str++;
-// 			j++;
-// 		}
-// 		else if (s[i] == c && s[i - 1] != c)
-// 		{
-// 			str2[j] = '\0';
-// 		}
-// 		if ((s[i] != c && s[i + 1] == c) || (s[i] != c && s[i + 1] == 0))
-// 		{
-// 			j = 0;
-// 			tab[k] = str2;
-// 			k++;
-// 			str2 = malloc(nextword((char *)s, c) + 1);
-// 		}
-// 		i++;
-// 	}
-// 	free(str2);
-// 	tab[k] = NULL;
-// 	printf("tab[0] : %s\n", tab[0]);
-// 	printf("tab[1] : %s\n", tab[1]);
-// 	printf("tab[2] : %s\n", tab[2]);
-// 	printf("tab[3] : %s\n", tab[3]);
-// 	return (tab);
-// }
